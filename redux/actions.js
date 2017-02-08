392af7b5a8620e6e1c1ca3896d2f1344f719377c
chrispaynes <cdpaynes@gmail.com>
2017-02-07 23:55:23 -0600

    Cleans up unneeded comments

--- redux/actions.js
+++ redux/actions.js
@@ -1,49 +1,39 @@
-//this file defines the actions as pure functions.
-//type: value is in caps, separated by underscore
-//every action has a type
-//type tells reducers how to handle the action
-//todo is built from the text string value of "text" property
-//completed will be set to false
-//ID will be set to the next number available
-//action is an object that gets return
-//the payload is additional info that helps the action get performed
-//components call this FN to activate an event
-//passed in text var is text typed in from input field
-//this actions are called as case statements in the reducer.js file
-
+/*
+actions represents a collection of pure functions that perform actions on data.
+Components call these actions to activate events. Each action.type will coordinate
+with a reducer event from reducer.js
+*/
 let actions = {
-  addTodo: function(text) {
+  addTodo: (text) => {
     return {
       type: 'ADD_TODO',
       text: text
     }
   },
 
-  //use keyID to bind a specific todo item to this method
-  completeTodo: function(id) {
+  // Adding ID as a property binds the id to the action.
+  completeTodo: (id) => {
     return {
       type: 'COMPLETE_TODO',
       id: id
     }
   },
 
-  deleteTodo: function(id) {
+  deleteTodo: (id) => {
     return {
       type: 'DELETE_TODO',
       id: id
     }
   },
 
-  createNewUserId: function() {
-    // do async server call
-    // onSuccess: dispatch action
+  createNewUserId: () => {
     return {
       type: 'CREATE_USER_ID',
       id: Math.floor(Math.random() * 100)
     }
   },
 
-  createNewUserIdIfOdd: function() {
+  createNewUserIdIfOdd: () => {
     return (dispatch, getState) => {
       const { user } = getState()
       if (user.id % 2 === 0) {
@@ -53,14 +43,15 @@ let actions = {
     }
   },
 
+  // setTimeout() mimics an async call by waiting to call the function.
   createNewUserIdAsync: function() {
     return (dispatch) => {
       setTimeout(() => {
-          dispatch(actions.createNewUserId())
-        }, 3000) //setTimeout() makes an async call. it takes a FN as 1st param, 2nd param is wait time
+        dispatch(actions.createNewUserId())
+      }, 3000)
     }
   }
 
 }
 
-export default actions
+export default actions;

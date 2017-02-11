18242108b9030718d3410113a018ad13f90565bb
chrispaynes <cdpaynes@gmail.com>
2017-02-10 22:15:07 -0600

    Adds basic CSS styling

--- client/client.js
+++ client/client.js
@@ -17,12 +17,12 @@
   surrounded/wrapped by the provider.
   The innermost app should have access to the states this.props
 */
-
 import React from 'react'
 import { render } from 'react-dom'
 import App from '../components/App'
 import configureStore from '../redux/store'
 import { Provider } from 'react-redux'
+require("./css/main.css");
 
 
 let initialState = {
--- webpack.config.js
+++ webpack.config.js
@@ -4,34 +4,37 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  devtool: 'inline-source-map', //inline-source-map displays console errors linked to source files.
+  // eval: Each module is executed with eval and //@ sourceURL.
+  // eval-source-map: Each module is executed with eval and a SourceMap is added as DataUrl to the eval
+  devtool: 'cheap-eval-source-map',
   context: path.join(__dirname),
   entry: {
     app: ['webpack-hot-middleware/client', './client/client.js']
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    // outputs custom bundle + hash for each entry point 
-    filename: '[name][hash]bundle.js'
+    // outputs custom bundle for each named entry point 
+    filename: '[name].bundle.js'
   },
   plugins: [
-       new webpack.optimize.OccurenceOrderPlugin(),
-       new webpack.HotModuleReplacementPlugin(),
-       new webpack.NoErrorsPlugin(),
-       new ExtractTextPlugin("bundle.css"),
-       new HtmlWebpackPlugin({
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin("bundle.css"),
+    new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.join(__dirname, "client", 'index.html'),
-      inject: true
+      inject: true,
+      hash: true
     })
-    ],
+  ],
 
   // Loaders receive query parameters via a query string (just like in the web).
   // Webpack appends the query string to the loader with ?. i.e. url-loader?mimetype=image/png.
   module: {
     loaders: [
       {
-        // JavaScript files
+        // JavaScript Loader
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
@@ -47,21 +50,28 @@ module.exports = {
         }
       },
       {
-        // HTML files
+        // HTML Loader
         test: /\.html$/,
         loader: 'raw-loader'
       },
       {
-        // CSS files
-        test: /client\css\/\.css$/,
-        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+        // CSS Loader
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'],
+        options: {
+          minimize: true
+        }
       }
     ]
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     inline: true,
-    stats: "errors-only"
+    stats: {
+      colors: true,
+      reasons: true,
+      chunks: false
+    }
   }
 
 }
